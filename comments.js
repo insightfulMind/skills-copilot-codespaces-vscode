2471b7704c7e432ea3ad8eff78303033d1f4b753
insightfulMind <140723294+insightfulMind@users.noreply.github.com>
2023-09-17 20:39:38 +0000

    Copilot second commit

---
+++ comments.js
@@ -0,0 +1,27 @@
+// Create web server to handle comments
+//
+// Run: node comments.js
+// 
+// Access: http://localhost:3000/comments
+// 
+// Access: http://localhost:3000/comments?user=1
+//
+// Access: http://localhost:3000/comments?user=1&password=1
+//
+// Access: http://localhost:3000/comments?user=1&password=1&comment=hello
+//
+// Access: http://localhost:3000/comments?user=1&password=1&comment=hello&image=1
+// 
+// Access: http://localhost:3000/comments?user=1&password=1&comment=hello&image=1&lat=37.7&lon=-122.4
+//
+// Access: http://localhost:3000/comments?user=1&password=1&comment=hello&image=1&lat=37.7&lon=-122.4&date=2015-03-25T12:00:00Z
+//
+// Access: http://localhost:3000/comments?user=1&password=1&comment=hello&image=1&lat=37.7&lon=-122.4&date=2015-03-25T12:00:00Z&rating=1
+//
+// Access: http://localhost:3000/comments?user=1&password=1&comment=hello&image=1&lat=37.7&lon=-122.4&date=2015-03-25T12:00:00Z&rating=1&tags=tag1,tag2
+// 
+// Access: http://localhost:3000/comments?user=1&password=1&comment=hello&image=1&lat=37.7&lon=-122.4&date=2015-03-25T12:00:00Z&rating=1&tags=tag1,tag2&radius=1000
+//
+// Access: http://localhost:3000/comments?user=1&password=1&comment=hello&image=1&lat=37.7&lon=-122.4&date=2015-03-25T12:00:00Z&rating=1&tags=tag1,tag2&radius=1000&limit=10
+//
+// Access: http://localhost:3000
